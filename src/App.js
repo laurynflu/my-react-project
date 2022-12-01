c89be3cc384527de1f5ca5006d8c5f5ed9cdd129
laurynflu <98038908+laurynflu@users.noreply.github.com>
2022-11-30 20:34:51 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import './styles.css';
 import Tuiter from "./components/tuiter";
@@ -5,6 +6,29 @@ import Tuiter from "./components/tuiter";
 function App() {
   return (
     <Tuiter/>
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+>>>>>>> 13b99110 (Initialize project using Create React App)
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 import { render, screen } from '@testing-library/react';
 import App from './App';
@@ -6,5 +7,13 @@ import '@testing-library/jest-dom'
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Home Screen/i);
+=======
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+>>>>>>> 13b99110 (Initialize project using Create React App)
   expect(linkElement).toBeInTheDocument();
 });
